b5f4be293c72d6b6cf3b30f080d5bf0890750ab7
emrcftci <emreciftci541@gmail.com>
2020-04-04 02:39:59 +0300

    refactor(*): Refactor controllers, AppDelegate, ViewModels and Cell

--- CoronaVirusMVVM/Views/MainTableViewCell/MainTableViewCell.swift
+++ CoronaVirusMVVM/Views/MainTableViewCell/MainTableViewCell.swift
@@ -5,13 +5,21 @@
 //  Created by Ali Emre Değirmenci on 2.04.2020.
 //  Copyright © 2020 Ali Emre Degirmenci. All rights reserved.
 //
-import Foundation
+
 import UIKit
 
 class MainTableViewCell: UITableViewCell {
 
-    @IBOutlet weak var countryLabel: UILabel!
-    @IBOutlet weak var deathsLabel: UILabel!
-    @IBOutlet weak var countryFlagImageView: UIImageView!
-    
+    @IBOutlet private weak var countryLabel: UILabel!
+    @IBOutlet private weak var deathsLabel: UILabel!
+    @IBOutlet private weak var countryFlagImageView: UIImageView!
+
+    func configure(with viewModel: CountryViewModel) {
+        countryLabel.text = viewModel.country
+        deathsLabel.text = "Deaths: \(viewModel.deaths)"
+
+        let imageURL = URL(string: viewModel.countryFlag)
+        countryFlagImageView.sd_setImage(with: imageURL,
+                                         placeholderImage: UIImage(named: "placeholder.png"))
+    }
 }
