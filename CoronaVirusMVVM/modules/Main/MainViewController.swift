03280b76d8db427fdee5283fabbafab51429047f
Ali Emre Değirmenci <degirmenci.a.emre@gmail.com>
2020-05-12 16:38:32 +0300

    Move UIImage extension to Extension folder

--- CoronaVirusMVVM/modules/Main/MainViewController.swift
+++ CoronaVirusMVVM/modules/Main/MainViewController.swift
@@ -230,11 +230,3 @@ extension MainViewController: MainViewModelDelegate {
 }
 
 
-extension UIImageView {
-    func setImage(with path: String?, placeholder: String = "placeholder") {
-        guard let path = path, let url = URL(string: path) else { return }
-        self.sd_setImage(with: url, placeholderImage: UIImage(named: placeholder))
-    }
-}
-
-
