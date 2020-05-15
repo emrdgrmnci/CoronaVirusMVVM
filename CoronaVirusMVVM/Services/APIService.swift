e0a6824fd9daf2a972ce1c4384da58ece3d28d33
Ali Emre Değirmenci <degirmenci.a.emre@gmail.com>
2020-05-15 14:19:51 +0300

    Code indent in APIService.swift file

--- CoronaVirusMVVM/Services/APIService.swift
+++ CoronaVirusMVVM/Services/APIService.swift
@@ -15,54 +15,41 @@ protocol APIServiceProtocol {
 }
 
 class APIService: APIServiceProtocol {
-
     func getCountries(url: URL, completion: @escaping CallBack<[Country]?>) {
-
         URLSession.shared.dataTask(with: url) { data, response, error in
-
             if let error = error {
                 print(error.localizedDescription)
                 completion(nil)
             } else if let data = data {
-
                 let countryList = try? JSONDecoder().decode([Country].self, from: data)
                 completion(countryList)
             }
-
         }.resume()
 
     }
 
     func getGlobalCases(url: URL, completion: @escaping CallBack<Global?>) {
-
         URLSession.shared.dataTask(with: url) { data, response, error in
-
             if let error = error {
                 print(error.localizedDescription)
                 completion(nil)
             } else if let data = data {
-
                 let global = try? JSONDecoder().decode(Global.self, from: data)
                 if let global = global {
                     completion(global)
                     print(global)
                 }
             }
-
         }.resume()
-
     }
 
     func getNews(url: URL, completion: @escaping CallBack<NewsResponse?>) {
-
         URLSession.shared.dataTask(with: url) { data, response, error in
-
             if let error = error {
                 print(error.localizedDescription)
             }
             else if let data = data {
                 let newsList = try? JSONDecoder().decode(NewsResponse.self, from: data)
-                
                 if let newsList = newsList {
                     completion(newsList)
                 }
