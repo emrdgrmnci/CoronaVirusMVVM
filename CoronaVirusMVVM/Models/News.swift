07c74983599ecdd17c930ddf5f985e31d7c90abb
Ali Emre Değirmenci <degirmenci.a.emre@gmail.com>
2020-05-20 14:55:47 +0300

    Delete unnecessary lines from News.swift

--- CoronaVirusMVVM/Models/News.swift
+++ CoronaVirusMVVM/Models/News.swift
@@ -14,7 +14,6 @@ struct NewsResponse : Codable {
     let news : [News]?
 
     enum CodingKeys: String, CodingKey {
-
         case status = "status"
         case totalResults = "totalResults"
         case news = "articles"
@@ -30,47 +29,10 @@ struct News: Codable {
     let urlToImage : String?
     let publishedAt : String?
     let content : String?
-
-    enum CodingKeys: String, CodingKey {
-
-        case source = "source"
-        case author = "author"
-        case title = "title"
-        case description = "description"
-        case url = "url"
-        case urlToImage = "urlToImage"
-        case publishedAt = "publishedAt"
-        case content = "content"
-    }
-
-    init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        source = try values.decodeIfPresent(Source.self, forKey: .source)
-        author = try values.decodeIfPresent(String.self, forKey: .author)
-        title = try values.decodeIfPresent(String.self, forKey: .title)
-        description = try values.decodeIfPresent(String.self, forKey: .description)
-        url = try values.decodeIfPresent(String.self, forKey: .url)
-        urlToImage = try values.decodeIfPresent(String.self, forKey: .urlToImage)
-        publishedAt = try values.decodeIfPresent(String.self, forKey: .publishedAt)
-        content = try values.decodeIfPresent(String.self, forKey: .content)
-    }
-
 }
 
 // MARK: - Source
 struct Source : Codable {
     let id : String?
     let name : String?
-
-    enum CodingKeys: String, CodingKey {
-
-        case id = "id"
-        case name = "name"
-    }
-
-    init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        id = try values.decodeIfPresent(String.self, forKey: .id)
-        name = try values.decodeIfPresent(String.self, forKey: .name)
-    }
 }
