c187f22660e31b6de0a38fdf5ccd5db6e8076d92
Ali Emre Değirmenci <degirmenci.a.emre@gmail.com>
2020-05-20 15:00:06 +0300

    Add Unit Test target

---
+++ CoronaVirusMVVMTests/CoronaVirusMVVMTests.swift
@@ -0,0 +1,25 @@
+//
+//  CoronaVirusMVVMTests.swift
+//  CoronaVirusMVVMTests
+//
+//  Created by Ali Emre Değirmenci on 20.05.2020.
+//  Copyright © 2020 Ali Emre Degirmenci. All rights reserved.
+//
+
+import XCTest
+
+class CoronaVirusMVVMTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+}
