945afd728adca5d31f72e55f5187e49461ce252e
Ali Emre Değirmenci <degirmenci.a.emre@gmail.com>
2020-05-20 16:58:00 +0300

    Change Models file name to DTOs

--- CoronaVirusMVVM/DTOs/Country.swift
+++ CoronaVirusMVVM/DTOs/Country.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-struct Country: Codable {
+ struct Country: Codable {
     let country: String?
     let countryInfo: CountryInfo?
     let cases: Int?
--- CoronaVirusMVVMTests/CoronaVirusMVVMTests.swift
+++ CoronaVirusMVVMTests/CoronaVirusMVVMTests.swift
@@ -7,15 +7,14 @@
 //
 
 import XCTest
+@testable import CoronaVirusMVVM
 
 class CoronaVirusMVVMTests: XCTestCase {
 
+    private var viewModel: MainViewModel!
+
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
     func testExample() throws {
